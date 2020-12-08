001ef968335e03c8dddbc6f54e9c3b4e0af5dbdb
mehmehmehlol <hkmeganlo@gmail.com>
2020-12-08 00:22:31 -0800

    css

--- src/Explore/ExploreDetails.js
+++ src/Explore/ExploreDetails.js
@@ -17,7 +17,7 @@ class ExploreDetails extends React.Component {
             <div className="explore-detail-container">
 
                 <div className="explore-detail">
-                    <h1>{name}</h1>
+                    <h1 className="explore-detail-title">{name}</h1>
                     <img src={image} 
                     style={{width: 500, height: "auto"}}
                     alt={name} />
@@ -31,11 +31,11 @@ class ExploreDetails extends React.Component {
                     <h3>More Information:</h3>
                    Website: 
                     <ExternalLink href={website} target="_blank" rel="noopener noreferrer"> 
-                        <span>{name}</span>
+                        <span style={{fontWeight: "300"}}>{name}</span>
                     </ExternalLink>
                     <br />
                     Address: {address}<br/>
-                    Phone Number: <a href="tel:{phone_number}">{phone_number}</a>
+                    Phone Number: <a href="tel:{phone_number}" style={{fontWeight: "300"}}>{phone_number}</a>
                     <div>
                         
                     {/* Tags: {selected.tags.map(tag => tag.name + " ")} */}
